0d6dd6b81c0713371cda14e7733c49b219a81b1c
Jeremy775 <jlaisne77@gmail.com>
2022-09-21 15:25:57 +0200

    changements

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-function Book(title, author, pages, read)
-{
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+let myLibrary = [
+    {
+        title : 'un livre',
+        author : 'quelqun',
+        pages : 2324,
+        read : true
+    }
+];
 
-    this.info = function() {
-        return (title + " by " + author + ", " + pages + " pages, " + read)
+myLibrary.forEach((book) => Book.showBooks(book))
+
+
+class Book {
+    constructor(title, author, pages, read)
+    {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
+    }
+
+
+    static addBook(title, author, pages, read)
+    {
+        let book = new Book(title, author, pages, read)
+        myLibrary.push(book)
+    }
+
+    static showBooks(book)
+    {
+        const list = document.querySelector('#book-list')
+
+        const row = document.createElement('tr')
+
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.pages}</td>
+            <td><a href="#" class="btn delete">X</a></td>
+        `;
+
+        list.appendChild(row)
     }
 }
 
-const TheHobbit = new Book('The Hobbit', 'JRR Tolkien', 1093, 'not read');
-console.log(TheHobbit.info());
+
+
+
+
